1504c6bafd35900442815af639124f43c3e5d44a
ArthurGoodman <arthur.goodman3@gmail.com>
2017-10-15 16:23:05 +0300

    Removed clang-format from install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,3 @@
 echo "source ~/.dotfiles/.vimrc" >> ~/.vimrc
 echo "source ~/.dotfiles/.bashrc" >> ~/.bashrc
 echo "source ~/.dotfiles/.gtkrc-2.0" >> ~/.gtkrc-2.0
-cp f .clang-format ~/.clang-format
