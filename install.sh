54400679d2d5141d6fd67b728f96b1e2f387d811
ArthurGoodman <arthur.goodman3@gmail.com>
2018-07-22 16:57:50 +0300

    Refactored install script; added new configs: i3, polybar, compton, ranger, mpd, dunst; added some utility scripts

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-echo "source ~/.dotfiles/.vimrc" >> ~/.vimrc
-echo "source ~/.dotfiles/.bashrc" >> ~/.bashrc
-echo "include \"$HOME/.dotfiles/.gtkrc-2.0\"" >> ~/.gtkrc-2.0
-echo "@import url(\"$HOME/.dotfiles/gtk.css\");" >> ~/.config/gtk-3.0/gtk.css
+#echo "include \"$HOME/.dotfiles/.gtkrc-2.0\"" >> ~/.gtkrc-2.0
+#echo "@import url(\"$HOME/.dotfiles/gtk.css\");" >> ~/.config/gtk-3.0/gtk.css
+
+ln -snf ~/.dotfiles/.vimrc ~/.vimrc
+ln -snf ~/.dotfiles/.bashrc.mine ~/.bashrc.mine
+ln -snf ~/.dotfiles/.profile.mine ~/.profile.mine
+
+cp -rsPf ~/.dotfiles/.config ~
+cp -rsPf ~/.dotfiles/.scripts ~
+
+echo "source $HOME/.bashrc.mine" >> ~/.bashrc
+echo "source $HOME/.profile.mine" >> ~/.profile
 
 mkdir -p ~/.vim/tmpdirs/undodir
