54b48e9cddccd9630923726aafd515bec55e91cd
ArthurGoodman <arthur.goodman3@gmail.com>
2018-07-26 00:53:42 +0300

    Implemented a check for first time installation in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#echo "include \"$HOME/.dotfiles/.gtkrc-2.0\"" >> ~/.gtkrc-2.0
-#echo "@import url(\"$HOME/.dotfiles/gtk.css\");" >> ~/.config/gtk-3.0/gtk.css
-
 ln -snf ~/.dotfiles/.vimrc ~/.vimrc
 ln -snf ~/.dotfiles/.bashrc.mine ~/.bashrc.mine
 ln -snf ~/.dotfiles/.profile.mine ~/.profile.mine
@@ -10,7 +7,16 @@ ln -snf ~/.dotfiles/.profile.mine ~/.profile.mine
 cp -rsPf ~/.dotfiles/.config ~
 cp -rsPf ~/.dotfiles/.scripts ~
 
-echo "source $HOME/.bashrc.mine" >> ~/.bashrc
-echo "source $HOME/.profile.mine" >> ~/.profile
+FLAG_FILE=~/.dotfiles_installed
 
-mkdir -p ~/.vim/tmpdirs/undodir
+if [ ! -f $FLAG_FILE ]; then
+    #echo "include \"$HOME/.dotfiles/.gtkrc-2.0\"" >> ~/.gtkrc-2.0
+    #echo "@import url(\"$HOME/.dotfiles/gtk.css\");" >> ~/.config/gtk-3.0/gtk.css
+
+    echo "source $HOME/.bashrc.mine" >> ~/.bashrc
+    echo "source $HOME/.profile.mine" >> ~/.profile
+
+    mkdir -p ~/.vim/tmpdirs/undodir
+fi
+
+touch $FLAG_FILE
