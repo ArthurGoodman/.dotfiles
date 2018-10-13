00929a8142b438c02384bc7d061333dba6269602
ArthurGoodman <arthur.goodman3@gmail.com>
2018-10-13 14:53:41 +0300

    Added basic urxvt config

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -snf ~/.dotfiles/.inputrc ~/.inputrc
 ln -snf ~/.dotfiles/.bashrc ~/.bashrc
 ln -snf ~/.dotfiles/.profile ~/.profile
 ln -snf .profile ~/.bash_profile
+ln -snf ~/.dotfiles/.Xresources ~/.Xresources
 
 cp -rsPf ~/.dotfiles/.config ~
 cp -rsPf ~/.dotfiles/.scripts ~
