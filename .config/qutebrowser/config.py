d7bbade0e1ab54e58af7fb53b4c8a5ecc1bc12e6
ArthurGoodman <arthur.goodman3@gmail.com>
2018-12-03 09:29:40 +0200

    Added qutebrowser config, assigned 3rd i3 workspace

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,6 @@
+config.load_autoconfig()
 config.source('nord.py')
+c.auto_save.session = True
+c.url.default_page = 'https://google.com'
+c.url.searchengines = {'DEFAULT': 'https://google.com/search?q={}'}
 c.url.start_pages = ['https://google.com']
-c.url.searchengines = {"DEFAULT": "https://google.com/search?q={}"}
