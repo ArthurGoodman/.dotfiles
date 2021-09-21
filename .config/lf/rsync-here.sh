2510b51e1274a2849dba8ab128c6ecd4ab73f04c
ArthurGoodman <arthur.goodman3@gmail.com>
2021-09-21 09:58:25 +0300

    Update lf config

--- .config/lf/rsync-here.sh
+++ .config/lf/rsync-here.sh
@@ -5,4 +5,6 @@ mode=$(echo "$load" | sed -n '1p')
 list=$(echo "$load" | sed '1d' | tr ' ' '*')
 if [ $mode = 'copy' ]; then
     rsync -HAXauvP $list ./
+    rm ~/.local/share/lf/files
+    lf -remote 'send clear'
 fi
